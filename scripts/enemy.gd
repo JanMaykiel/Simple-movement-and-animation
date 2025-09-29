215055312cfae24818a473f5eaae808da7df926c
JanMaykiel <michaelmendiola30@gmail.com>
2025-09-29 20:57:06 +0800

    added sliding mechanic

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,14 +3,16 @@ extends CharacterBody3D
 var original_color = "#7f5c17"
 var red = "#b91115"
 
-@onready var hit_timer = $Armature/Skeleton3D/Timer
 @onready var enemy_mesh = %Cube
 
-func _on_area_3d_area_shape_entered(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
-	var material = enemy_mesh.get_active_material(0)
-	material.albedo_color = red
-	print("hit")
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	if area.name == "weapon_hitbox":
+		var material = enemy_mesh.get_active_material(0)
+		material.albedo_color = red
+		print("hit")
 
-func _on_area_3d_area_shape_exited(area_rid: RID, area: Area3D, area_shape_index: int, local_shape_index: int) -> void:
-	var material = enemy_mesh.get_active_material(0)
-	material.albedo_color = original_color
+
+func _on_area_3d_area_exited(area: Area3D) -> void:
+	if area.name == "weapon_hitbox":
+		var material = enemy_mesh.get_active_material(0)
+		material.albedo_color = original_color
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-const GRAVITY = 9.8
-const SENSITIVITY = 1.0 / 500
-const LERP = 0.15
-const RAY_LENGTH = 1000
+const MAX_SPEED: float = 10.0
+const ACCELERATION: float = 3.0
+const DECELERATION: float = 50.0
+const JUMP_VELOCITY: float = 5.0
+const GRAVITY: float = 9.8
+const SENSITIVITY: float = 1.0 / 500
+const LERP: float = 0.15
+
+var current_speed: float = 0.0
+var drag = DECELERATION
 
 #Player
 @onready var parent = $"."
@@ -34,42 +38,62 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= GRAVITY * delta
 
-	#Handle Jumping and Movement
+	#Handle Jumping, Sliding and Movement
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	var input_dir = Input.get_vector("move_right","move_left","move_backward","move_forward")
 	var direction = (armature.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		current_speed = move_toward(current_speed, MAX_SPEED / 2, ACCELERATION * delta)
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = 0.0
-		velocity.z = 0.0
+		if not is_on_floor():
+			drag = DECELERATION / 5
+		current_speed = move_toward(current_speed, 0, drag * delta)
+		velocity.x = move_toward(velocity.x, 0, drag * delta)
+		velocity.z = move_toward(velocity.z, 0, drag * delta)
 
+	slide(delta, direction)
+	attack()
 	update_animation_parameters(delta)
 
 	move_and_slide()
 
-func attack():
-	var enemies = weapon_hitbox.get_overlapping_bodies()
-	for enemy in enemies:
-		if enemy.has_method("hurt"):
-			enemy.hurt()
-
-func update_animation_parameters(delta):
+func update_animation_parameters(delta: float):
 	#Blend the animation smoothly to avoid camera snapping constantly when moving
-	var target_blend_position = velocity.length() / SPEED
-	var current_blend_positon = anim_tree.get("parameters/StateMachine/BlendSpace1D/blend_position")
+	var target_blend_position = velocity.length() / 5
+	var current_blend_positon = anim_tree.get("parameters/StateMachine/run/blend_position")
 	current_blend_positon = lerp(current_blend_positon, target_blend_position, 5 * delta)
-	anim_tree.set("parameters/StateMachine/BlendSpace1D/blend_position", current_blend_positon)
-	
-	if(Input.is_action_just_pressed("attack")):
-		weapon_hitbox.disabled = false
+	anim_tree.set("parameters/StateMachine/run/blend_position", current_blend_positon)
+
+func attack():
+	if Input.is_action_just_pressed("attack"):
+		if Input.is_action_pressed("slide"):
+			anim_tree["parameters/StateMachine/conditions/slide_attack"] = true
 		anim_tree["parameters/StateMachine/conditions/attack"] = true
+		if anim_tree.get("parameters/StateMachine/conditions/attack") == true or anim_tree.get("parameters/StateMachine/conditions/slide_attack") == true:
+			weapon_hitbox.disabled = false
 	else:
 		weapon_hitbox.disabled = true
 		anim_tree["parameters/StateMachine/conditions/attack"] = false
+		anim_tree["parameters/StateMachine/conditions/slide_attack"] = false
+
+func slide(delta: float, direction: Vector3):
+	if direction and Input.is_action_pressed("slide") and is_on_floor():
+		anim_tree["parameters/StateMachine/conditions/not_sliding"] = false
+		anim_tree["parameters/StateMachine/conditions/sliding"] = true
+		var target_blend_position = velocity.length() / 5
+		var current_blend_positon = anim_tree.get("parameters/StateMachine/slide/blend_position")
+		current_blend_positon = lerp(current_blend_positon, target_blend_position, 5 * delta)
+		anim_tree.set("parameters/StateMachine/slide/blend_position", current_blend_positon)
+		current_speed = move_toward(current_speed, MAX_SPEED, ((ACCELERATION * 4) * delta))
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	if (direction.x == 0 and direction.z == 0) or Input.is_action_just_released("slide") or not is_on_floor():
+		anim_tree["parameters/StateMachine/conditions/not_sliding"] = true
+		anim_tree["parameters/StateMachine/conditions/sliding"] = false
 
 #func raycast_check_intersect():
 	#var object_hit = raycast.get_collider()
